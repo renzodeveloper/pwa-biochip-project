5ce73abd7a978303339f42175c82f28d6ee8da8b
renzodeveloper <rko2007hn@gmail.com>
2026-05-02 19:21:44 -0500

    feat(PRO-002): Updating the Typescript configuration

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+    throw new Error('Root element not found');
+}
+
+ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+        <h1>Hello, PWA Biochip Project!</h1>
+    </React.StrictMode>
+)
