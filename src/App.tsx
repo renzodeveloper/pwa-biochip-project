80b6932e1ad55b6116149b827920074f9401ea5b
renzodeveloper <rko2007hn@gmail.com>
2026-05-02 19:35:32 -0500

    feat(PRO-002): Adding App component

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+    return (
+        <>
+            <h1>PWA Biochip Project</h1>
+        </>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './main.css';
+import { App } from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -9,6 +10,6 @@ if (!rootElement) {
 
 ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-        <h1>Hello, PWA Biochip Project!</h1>
+        <App />
     </React.StrictMode>
 )
